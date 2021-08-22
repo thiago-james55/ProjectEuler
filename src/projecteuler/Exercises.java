bf4632d3d6e6558b6fbf6a1cf42b8cfc38e56994
Thiago Gonçalves Souza <Pichau@TGS-13-CAMORRA>
2021-08-22 19:56:33 -0300

    Exercício 11 Resolvido!

--- src/projecteuler/Exercises.java
+++ src/projecteuler/Exercises.java
@@ -7,6 +7,7 @@ package projecteuler;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -717,4 +718,373 @@ public class Exercises {
             System.out.println("A soma de numeros primos antes de 2 milhoes é : " + total);
         
     }//E10
+    
+    
+    public static void E11() {
+        
+        /*
+        Largest product in a grid
+
+        Problem 11
+        In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+
+        08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+        49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+        81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+        52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+        22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+        24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+        32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+        67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+        24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+        21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+        78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+        16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+        86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+        19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+        04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+        88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+        04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+        20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+        20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+        01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+
+        The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+
+        What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+        */
+        
+        //Tirar os espaços da String
+                           // 0 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19
+                           
+        String gridString = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08" + //0
+                            "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00" + //1
+                            "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65" + //2
+                            "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91" + //3
+                            "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80" + //4
+                            "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50" + //5
+                            "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70" + //6
+                            "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21" + //7
+                            "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72" + //8
+                            "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95" + //9
+                            "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92" + //10
+                            "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57" + //11
+                            "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58" + //12
+                            "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40" + //13
+                            "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66" + //14
+                            "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69" + //15
+                            "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36" + //16
+                            "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16" + //17
+                            "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54" + //18
+                            "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"; //19
+        
+        gridString = gridString.replace(" ","");
+        
+               
+        int[][] grid = new int[20][20];
+              
+        //Auto atribuição da grid no Array Matriz
+        
+        String localNumber;
+        int gridLength = 0;
+        
+        for (int linha = 0; linha < 20; linha++) {
+            
+            
+            for (int coluna = 0; coluna < 20; coluna++) {
+                    
+                    localNumber = "" + gridString.charAt(gridLength) + gridString.charAt(gridLength+1);
+                    grid[linha][coluna] = Integer.parseInt(localNumber);
+                                                            
+                    gridLength += 2;
+               
+            }
+            
+        }
+        
+                
+        //Varredura do Array da direita pra esquerda e da esquerda para direita
+        
+        int maxEsquerdaDireita = 0;
+        
+        String combinacaoMaiorEsquerdaDireta = "";
+        
+        int n1 = 0;
+        int n2 = 0;
+        int n3 = 0;
+        int n4 = 0;
+               
+        
+        for (int linha = 0; linha < grid.length; linha++) {
+            
+            for (int coluna = 0; coluna < grid.length; coluna++) {
+                
+                if (linha != grid.length-1) { // verifica se esta na ultima linha para nao fazer o procedimento de somar os da linha de baixo
+                    
+                    if ( (coluna == (grid.length-3)) || (coluna == (grid.length-2)) || (coluna == (grid.length-1)) ) { // verifica se esta alem da linha
+
+                        switch (coluna) { // verifica qual posicao esta para pular para linha de baixo                    
+
+                            case 17 :
+
+                                n1 = grid[linha][coluna];
+                                n2 = grid[linha][coluna+1];
+                                n3 = grid[linha][coluna+2];
+                                n4 = grid[linha+1][0];
+                                break;
+
+                            case 18 :
+
+                                n1 = grid[linha][coluna];
+                                n2 = grid[linha][coluna+1];
+                                n3 = grid[linha+1][0];
+                                n4 = grid[linha+1][1];
+                                break;
+
+                            case 19 :
+
+                                n1 = grid[linha][coluna];
+                                n2 = grid[linha+1][0];
+                                n3 = grid[linha+1][1];
+                                n4 = grid[linha+1][2];
+                                break;
+                        }
+                        
+                    } else {
+
+                        n1 = grid[linha][coluna];
+                        n2 = grid[linha][coluna+1];
+                        n3 = grid[linha][coluna+2];
+                        n4 = grid[linha][coluna+3];
+                    }
+                
+                            
+                } else if (coluna == grid.length-3) { // se a coluna for igual a 17 da break pois se nao tera +4 par o lado
+                    
+                    break;
+                    
+                    
+                } else  {
+                    
+                    n1 = grid[linha][coluna];
+                    n2 = grid[linha][coluna+1];
+                    n3 = grid[linha][coluna+2];
+                    n4 = grid[linha][coluna+3];
+                    
+                }
+                
+                // executa a soma do produto
+                
+                int direitaPraEsquerda,EsquerdaPraDireita,maiorLocalEsquerdaDireita;
+                
+                direitaPraEsquerda = n1*n2*n3*n4;
+                EsquerdaPraDireita = n4*n3*n2*n1;
+                
+                maiorLocalEsquerdaDireita = direitaPraEsquerda > EsquerdaPraDireita ? direitaPraEsquerda : EsquerdaPraDireita; // operador ternario para comparação local
+                
+                if (maiorLocalEsquerdaDireita > maxEsquerdaDireita) {
+                    
+                    maxEsquerdaDireita = maiorLocalEsquerdaDireita;
+                    
+                    combinacaoMaiorEsquerdaDireta = "Maior Produto esta na Posicão: Linha[" + linha + "] e coluna [" + coluna + "] ";
+                    
+                    if (direitaPraEsquerda > EsquerdaPraDireita) {
+                        
+                        combinacaoMaiorEsquerdaDireta += "Direita pra esquerda : " + n1 + "*" + n2 + "*" + n3 + "*" + n4;
+                                
+                    } else {
+                        
+                        combinacaoMaiorEsquerdaDireta += "Esquerda pra Direita : " + n1 + "*" + n2 + "*" + n3 + "*" + n4;
+                                
+                    }
+                    
+                }
+                
+                
+                
+                
+                
+                
+                
+            } // for coluna
+            
+        } // Final da Varredura esquerda pra direita e vice versa // for linha
+        
+        System.out.println(combinacaoMaiorEsquerdaDireta + " = " + maxEsquerdaDireita);
+        
+        // Varredura de cima pra baixo e de baixo pra cima
+        
+        int maxCimaPraBaixo = 0;
+        
+        String combinacaoMaiorCimaBaixo = "";
+        
+        for (int linha = 0; linha < grid.length; linha++) {
+            
+            if(linha == grid.length-3) { // se tiver na posição 17 da break pois nao vai ter a posição no Array
+                break;
+            }
+            
+            for (int coluna = 0; coluna < grid.length; coluna++) {
+                
+                n1 = grid[linha][coluna];
+                n2 = grid[linha+1][coluna];
+                n3 = grid[linha+2][coluna];
+                n4 = grid[linha+3][coluna];
+                
+                
+                
+                int cimaPrabaixo,baixoPracima,maiorLocalcimaPraBaixo;
+
+                    cimaPrabaixo = n1*n2*n3*n4;
+                    baixoPracima = n4*n3*n2*n1;
+
+                    maiorLocalcimaPraBaixo = cimaPrabaixo > baixoPracima ? cimaPrabaixo : baixoPracima; // operador ternario para comparação local
+
+                    if (maiorLocalcimaPraBaixo > maxCimaPraBaixo) {
+
+                        maxCimaPraBaixo = maiorLocalcimaPraBaixo;
+
+                        combinacaoMaiorCimaBaixo = "Maior Produto esta na Posicão: Linha[" + linha + "] e coluna [" + coluna + "] ";
+
+                        if (cimaPrabaixo > baixoPracima) {
+
+                            combinacaoMaiorCimaBaixo += "Cima para Baixo : " + n1 + "*" + n2 + "*" + n3 + "*" + n4;
+
+                        } else {
+
+                            combinacaoMaiorCimaBaixo += "Baixo para Cima : " + n4 + "*" + n3 + "*" + n2 + "*" + n1;
+
+                        }
+
+                    }
+            }
+            
+            
+        } 
+        
+        System.out.println(combinacaoMaiorCimaBaixo + " = " + maxCimaPraBaixo);
+        // final varredura cima para baixo e baixo para cima
+        
+        //Inicio Varredura de diagonal esquerda para direita
+        
+        int maxDiagonalEsquerda = 0;
+        
+        String combinacaoMaiorDiagonalEsquerda = "";
+        
+        for (int linha = 0; linha < grid.length-3; linha++) {
+            
+            for (int coluna = 0; coluna < grid.length-3; coluna++) {
+                
+                if (linha == grid.length-3) {
+                    
+                    break;
+                    
+                } else {
+                    
+                n1 = grid[linha][coluna];
+                n2 = grid[linha+1][coluna+1];
+                n3 = grid[linha+2][coluna+2];
+                n4 = grid[linha+3][coluna+3];
+                
+                }
+                
+                int diagonalCimaPrabaixo,diagonalBaixoPracima,maiorLocalDiagonalEsquerdaParaDireita;
+
+                    diagonalCimaPrabaixo = n1*n2*n3*n4;
+                    diagonalBaixoPracima = n4*n3*n2*n1;
+
+                    maiorLocalDiagonalEsquerdaParaDireita = diagonalCimaPrabaixo > diagonalBaixoPracima ? diagonalCimaPrabaixo : diagonalBaixoPracima; // operador ternario para comparação local
+
+                    if (maiorLocalDiagonalEsquerdaParaDireita > maxDiagonalEsquerda) {
+
+                        maxDiagonalEsquerda = maiorLocalDiagonalEsquerdaParaDireita;
+
+                        combinacaoMaiorDiagonalEsquerda  = "Maior Produto esta na Posicão: Linha[" + linha + "] e coluna [" + coluna + "] ";
+
+                        if (diagonalCimaPrabaixo > diagonalBaixoPracima) {
+
+                            combinacaoMaiorDiagonalEsquerda  += "Diagonal Esquerda Cima para Baixo : " + n1 + "*" + n2 + "*" + n3 + "*" + n4;
+
+                        } else {
+
+                            combinacaoMaiorDiagonalEsquerda  += "Diagonal Esquerda Baixo para Cima : " + n4 + "*" + n3 + "*" + n2 + "*" + n1;
+
+                        }
+
+                    }
+            
+                
+            } // coluna
+            
+            
+        } // linha
+        
+        System.out.println(combinacaoMaiorDiagonalEsquerda + " = " + maxDiagonalEsquerda);
+        
+        // final varredura Diagonal Esquerda
+        
+        //Inicio Varredura de diagonal Direita para Esquerda
+        
+        int maxDiagonalDireita = 0;
+        
+        String combinacaoMaiorDiagonalDireita = "";
+        
+        for (int linha = 0; linha < grid.length-3; linha++) {
+            
+            for (int coluna = 3; coluna < grid.length; coluna++) {
+                
+                if (linha == grid.length-3) {
+                    
+                    break;
+                    
+                } else {
+                    
+                n1 = grid[linha][coluna];
+                n2 = grid[linha+1][coluna-1];
+                n3 = grid[linha+2][coluna-2];
+                n4 = grid[linha+3][coluna-3];
+                
+                   
+                               
+                
+                
+                int diagonalDireitaCimaPrabaixo,diagonalDireitaBaixoPracima,maiorLocalDiagonalDireitaParaEsquerda;
+
+                    diagonalDireitaCimaPrabaixo = n1*n2*n3*n4;
+                    diagonalDireitaBaixoPracima = n4*n3*n2*n1;
+
+                    maiorLocalDiagonalDireitaParaEsquerda = diagonalDireitaCimaPrabaixo > diagonalDireitaBaixoPracima ? diagonalDireitaCimaPrabaixo : diagonalDireitaBaixoPracima; // operador ternario para comparação local
+
+                    if (maiorLocalDiagonalDireitaParaEsquerda > maxDiagonalDireita) {
+
+                        maxDiagonalDireita = maiorLocalDiagonalDireitaParaEsquerda;
+
+                        combinacaoMaiorDiagonalDireita  = "Maior Produto esta na Posicão: Linha[" + linha + "] e coluna [" + coluna + "] ";
+
+                        if (diagonalDireitaCimaPrabaixo > diagonalDireitaBaixoPracima) {
+
+                            combinacaoMaiorDiagonalDireita  += "Diagonal Direita Cima para Baixo : " + n1 + "*" + n2 + "*" + n3 + "*" + n4;
+
+                        } else {
+
+                            combinacaoMaiorDiagonalDireita  += "Diagonal Direita Baixo para Cima : " + n4 + "*" + n3 + "*" + n2 + "*" + n1;
+
+                        }
+
+                    }
+            
+                
+            } // coluna
+            
+            }    
+        } // linha
+        
+        System.out.println(combinacaoMaiorDiagonalDireita + " = " + maxDiagonalDireita);
+        
+        // final varredura Diagonal Direita
+        
+        
+        
+        
+    }
 }
